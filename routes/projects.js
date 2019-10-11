a2158b3d2945690b1646aa3b6b39e75cff23a484
EliBildman <esbildman@gmail.com>
2019-10-11 17:38:09 -0400

    added compiler to maze

--- routes/projects.js
+++ routes/projects.js
@@ -94,16 +94,22 @@ router.get('/maze', (req, res, next) => {
 
 router.post('/maze/solve', (req, res, next) => {
     var args = JSON.parse(req.body["data"]);
-    //args.unshift(path.join(__dirname, "..", "external_projects", "MazeSolver", "solution.json"));
-    var route = path.join(__dirname, "..", "external_projects", "MazeSolver", "MazeSolver");
-    //console.log("hello");
-    var proc = spawn(route, args);
-    var ans = "";
-    proc.stdout.on('data', (d) => ans += d.toString());
-    proc.on('exit', () => res.end(JSON.stringify(formatNLCN(ans))));
-    // proc.on("exit", () => {
-    //     res.end(fs.readFileSync(args[0]));
-    // });
+
+    var route = path.join(__dirname, "..", "external_projects", "MazeSolver", "build", "MazeSolver");
+
+    var solve = () => {
+        var proc = spawn(route, args);
+        var ans = "";
+        proc.stdout.on('data', (d) => ans += d.toString());
+        proc.on('exit', () => res.end(JSON.stringify(formatNLCN(ans))));
+    }
+
+    if(!fs.existsSync(route)) {
+        var buildProc = spawn('make', ['-C', path.join(__dirname, '..', 'external_projects', 'MazeSolver')]);
+        buildProc.on('exit', () => {solve()});
+    } else {
+        solve();
+    }
 
 });
 
